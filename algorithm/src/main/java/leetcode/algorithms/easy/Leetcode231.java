6d491553e3fabbef0379fec774429065e748076e
panweiming <10019818@Shein-inc.com>
2021-05-30 13:32:18 +0800

    'faker'

--- algorithm/src/main/java/leetcode/algorithms/easy/Leetcode231.java
+++ algorithm/src/main/java/leetcode/algorithms/easy/Leetcode231.java
@@ -26,6 +26,7 @@ public class Leetcode231 {
     public boolean isPowerOfTwo2(int n){
         return n > 0 && (n & (n-1)) == 0;
     }
+
     public boolean isPowerOfTwo(int n) {
         if(n == 1){
             return true;
